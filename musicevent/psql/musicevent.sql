5416a269fa6a039f30eb0585e19e390bec7495e2
val14-98 <valentin.eloy@dbmail.com>
2020-11-22 15:21:14 +0100

    Ajout contenu base de donnée

--- musicevent/psql/musicevent.sql
+++ musicevent/psql/musicevent.sql
@@ -19,7 +19,7 @@ CREATE TABLE MusicType
     name varchar (100) NOT NULL
 );
 
-INSERT INTO MusicType (name) VALUES ('Metal'),('Power Metal'), ('Classic'), ('Jazz'), ('Electro');
+INSERT INTO MusicType (name) VALUES ('Metal'),('Rock'), ('Classic'), ('Jazz'), ('Electro');
 
 CREATE TABLE Band
 (
@@ -30,7 +30,11 @@ CREATE TABLE Band
     FOREIGN KEY (idMusicType) REFERENCES MusicType(id)
 );
 
-INSERT INTO Band (idMusicType, name) VALUES (2,'Powerwolf');
+INSERT INTO Band (idMusicType, name, mediaUrl) VALUES (1,'Powerwolf','https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
+INSERT INTO Band (idMusicType, name, mediaUrl) VALUES (1,'Sabaton','https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515371_10629.jpg');
+INSERT INTO Band (idMusicType, name, mediaUrl) VALUES (1,'System Of A Down','https://www.lagrosseradio.com/webzine/images/15743.jpg');
+INSERT INTO Band (idMusicType, name, mediaUrl) VALUES (2,'ACDC','https://i.ytimg.com/vi/9FzC5881E4s/maxresdefault.jpg');
+INSERT INTO Band (idMusicType, name, mediaUrl) VALUES (1,'As I Lay Dying','https://www.rockurlife.net/wp-content/uploads/2019/09/ShapedByFIre.jpg');
 
 CREATE TABLE Country
 (
@@ -38,7 +42,7 @@ CREATE TABLE Country
     name varchar (50) NOT NULL
 );
 
-INSERT INTO Country (name) VALUES ('France'),('Allemagne');
+INSERT INTO Country (name) VALUES ('France'),('Deutschland'),('Belgium'),('Switzerland'),('Italy'),('Spain'),('United Kingdom');
 
 CREATE TABLE City
 (
@@ -49,6 +53,12 @@ CREATE TABLE City
 );
 
 INSERT INTO City (idCountry, name) VALUES (1, 'Paris'), (1, 'Marseille'), (1, 'Toulouse'), (1, 'Bordeaux');
+INSERT INTO City (idCountry, name) VALUES (2, 'Berlin'), (2, 'Munich'), (2, 'Hambourg'), (2, 'Cologne');
+INSERT INTO City (idCountry, name) VALUES (3, 'Liège'), (3, 'Bruxelles'), (3, 'Bruges'), (3, 'Anvers');
+INSERT INTO City (idCountry, name) VALUES (4, 'Zurich'), (4, 'Genève'), (4, 'Lausanne'), (4, 'Montreux');
+INSERT INTO City (idCountry, name) VALUES (5, 'Rome'), (5, 'Venise'), (5, 'Florence'), (5, 'Milan');
+INSERT INTO City (idCountry, name) VALUES (6, 'Madrid'), (6, 'Barcelone'), (6, 'Valence'), (6, 'Séville');
+INSERT INTO City (idCountry, name) VALUES (7, 'London'), (7, 'Manchester'), (7, 'Liverpool'), (7, 'Edimbourg');
 
 CREATE TABLE Concert
 (
