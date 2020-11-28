13171495ef30be2aa78a7831493fbc6792f5b9b6
Your Name <you@example.com>
2020-11-28 14:17:12 +0100

    Ajout de commentaires

--- musicevent/app/app.py
+++ musicevent/app/app.py
@@ -2,70 +2,92 @@ from flask import Flask, render_template, request
 import manager
 
 # TODO : commenter et détailler __name__
-# TODO : renommer variables state, etc...
 
 app = Flask(__name__)
 
 
-
-@app.route('/', methods=['GET'])
+# Définition de l'URL qui déclenche l'appel de la fonction
+@app.route('/', methods=['GET'])  
+# Récupère les données à afficher dans la page d'accueil et les passe à la page index.hml
 def accueil():
-    query_1_state, concerts = manager.getMainPageConcerts()
-    query_2_state, lastResa = manager.getLastReservedConcerts()
+    # Récupération des données
+    query_1_state, concerts               = manager.getMainPageConcerts()
+    query_2_state, lastlyReservedConcerts = manager.getLastReservedConcerts()
+
+    # Passage des données à la page Web
     return render_template('index.html',
                             queryState = query_1_state and query_2_state,
                             concertsList=concerts,
-                            lastBookedConcerts=lastResa)
+                            lastBookedConcerts=lastlyReservedConcerts)
 
 
+# Définition de l'URL qui déclenche l'appel de la fonction
 @app.route('/book', methods=['GET'])
+# Réserve une place à un concert pour l'utilisateur
 def book():
+    # Récupération des paramètres
     idConcert = request.args.get('idConcert')
     firstName = request.args.get('firstName')
     lastName = request.args.get('lastName')
     email = request.args.get('email')
 
-    state                   = manager.book(idConcert, firstName, lastName, email)
-    query_1_state, concerts = manager.getMainPageConcerts()
-    query_2_state, lastResa = manager.getLastReservedConcerts()
+    # Récupération des données
+    query_1_state                         = manager.book(idConcert, firstName, lastName, email)
+    query_2_state, concerts               = manager.getMainPageConcerts()
+    query_3_state, lastlyReservedConcerts = manager.getLastReservedConcerts()
 
+    # Passage des données à la page Web
     return render_template('index.html',
-                            queryState = state and query_1_state and query_2_state,
+                            queryState = query_1_state and query_2_state and query_3_state,
                             concertsList=concerts,
-                            lastBookedConcerts=lastResa)
+                            lastBookedConcerts=lastlyReservedConcerts)
 
 
+
+# Définition de l'URL qui déclenche l'appel de la fonction
 @app.route('/search', methods=['GET'])
+# Récupère les données à afficher selon les paramètres de la recherche et les passe à la page index.hml
 def search():
+    # Récupération des paramètres
     searchMethod = request.args.get('method')
     value = request.args.get('value')
+
+    # Récupération des données
     if searchMethod == "city":
-        state, concerts = manager.getConcertsByCity(value)
+        query_1_state, concerts = manager.getConcertsByCity(value)
     elif searchMethod == "date":
-        state, concerts = manager.getConcertsByDate(value)
+        query_1_state, concerts = manager.getConcertsByDate(value)
     else:
-        state, concerts = manager.getConcertsByArtist(value)
+        query_1_state, concerts = manager.getConcertsByArtist(value)
 
-    query_1_state, lastResa = manager.getLastReservedConcerts()
+    query_2_state, lastlyReservedConcerts = manager.getLastReservedConcerts()
+
+    # Passage des données à la page Web
     return render_template('index.html',
-                            queryState = state and query_1_state,
+                            queryState = query_1_state and query_2_state,
                             concertsList=concerts,
-                            lastBookedConcerts=lastResa)
+                            lastBookedConcerts=lastlyReservedConcerts)
 
 
 
-
+# Définition de l'URL qui déclenche l'appel de la fonction
 @app.route('/subscribe', methods=['GET'])
+# Ajoute une adresse e-mail à la liste des e-mail souhaitant recevoir la newsletter
 def subscribeToNewsletter():
+    # Récupération des paramètres
     email = request.args.get('email')
     offers = request.args.get('offers')
-    state = manager.subscribeToNewsletter(email, offers)
-    query_1_state, concerts = manager.getMainPageConcerts()
-    query_2_state, lastResa = manager.getLastReservedConcerts()
+
+    # Récupération des données
+    query_1_state                         = manager.subscribeToNewsletter(email, offers)
+    query_2_state, concerts               = manager.getMainPageConcerts()
+    query_3_state, lastlyReservedConcerts = manager.getLastReservedConcerts()
+
+    # Passage des données à la page Web
     return render_template('index.html',
-                            queryState = state and query_1_state and query_2_state,
+                            queryState = query_1_state and query_2_state and query_3_state,
                             concertsList=concerts,
-                            lastBookedConcerts=lastResa)
+                            lastBookedConcerts=lastlyReservedConcerts)
 
 
 
